6ce17d8797e1e0e1789e70969a8612a713897d45
Cheke <cheke@example.com>
2024-04-08 12:28:39 -0300

    ++

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const 
 app = require('./app'),
-port = process.env.PORT || 3000;
+port = process.env.PORT || 5000;
 
 app.listen(port,() =>{
     console.log(`Server Running:  http://localhost:${port}`)
